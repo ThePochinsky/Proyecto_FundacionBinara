6fe35fcbe361e78a560e3f325106be29762facfa
Christian1910n <christian.garaicoa.est@tecazuay.edu.ec>
2023-08-16 20:33:04 -0500

    Ventana emergente formulario de creacion de proyecto

--- src/app/modules/users/proyectos/pages/proyectos/proyectos.component.ts
+++ src/app/modules/users/proyectos/pages/proyectos/proyectos.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { RegisterProjectComponent } from 'src/app/modules/manager/register-project/pages/register-project/register-project.component';
 
 @Component({
   selector: 'app-proyectos',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./proyectos.component.css']
 })
 export class ProyectosComponent {
+  constructor(private bottomSheet: MatBottomSheet) {}
 
+  abrirSheet() {
+    const sheetRef = this.bottomSheet.open(RegisterProjectComponent, {
+      // puedes pasar datos al sheet así:
+      // data: {nombre: 'Ejemplo', descripcion: 'Datos'}
+    });
+
+    sheetRef.afterDismissed().subscribe(() => {
+      console.log('El bottom sheet se cerró');
+    });
+  }
 }
--- src/app/modules/users/proyectos/proyectos-routing.module.ts
+++ src/app/modules/users/proyectos/proyectos-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 
 const routes: Routes = [
   {
@@ -10,7 +11,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    RouterModule.forChild(routes),
+    MatBottomSheetModule
+  ],
   exports: [RouterModule]
 })
 export class ProyectosRoutingModule { }
+
