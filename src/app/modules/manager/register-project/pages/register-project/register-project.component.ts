4341deadfca3fa3cad2cd5be962e4664112d9c34
Jaqueline11 <miriancutiopala123@gmail.com>
2023-08-17 10:39:51 -0500

    solo numeros

--- src/app/modules/manager/register-project/pages/register-project/register-project.component.ts
+++ src/app/modules/manager/register-project/pages/register-project/register-project.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-project.component.css']
 })
 export class RegisterProjectComponent {
-
+  Solonumero(event: any) {
+    const input = event.target as HTMLInputElement;
+    let value = input.value;
+    value = value.replace(/[^0-9.]/g, '');
+    const decimalCount = value.split('.').length - 1;
+    if (decimalCount > 1) {
+      value = value.replace(/\.+$/, '');
+    }
+    input.value = value;
+  }
 }
