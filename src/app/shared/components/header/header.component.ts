3235752655d9bf337a0b11f873633c2b067abb46
BryanCabrera09 <81339678+BryanCabrera09@users.noreply.github.com>
2023-08-19 14:25:46 -0500

    Update Navbar Resposive

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -9,14 +9,15 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   isMenuOpen: boolean = false;
+  selectedLink: string = '';
 
   constructor(private router: Router) { }
 
   ngOnInit() { }
 
-  selectedLink: string = '';
   backgroundImages: { [key: string]: string } = {
-    '': 'url("https://tirant.com/wp-content/uploads/2023/06/dia-mundial-del-medio-ambiente-face.png")',
+    '': 'url("assets/img/portada_binara.png")',
+    'Home': 'url("assets/img/portada_binara.png")',
     'Nosotros': 'url("https://di-sitebuilder-assets.s3.amazonaws.com/GMimages/gmMLP/chevrolet/Corvette/2023/Content-1.jpg")',
     'Proyectos': 'url("https://di-sitebuilder-assets.s3.amazonaws.com/GMimages/gmMLP/chevrolet/Corvette/2023/Content-1.jpg")',
     'Anuncios': 'url("../assets/images/anuncios-background.jpg")',
@@ -29,9 +30,26 @@ export class HeaderComponent implements OnInit {
     this.selectedLink = link;
   }
 
-  goToProjects() {
-    this.changeBackground('Proyectos');
-    this.router.navigate(['usuario/proyectos']);
+  toggleMenu() {
+    const toggleBtn = document.querySelector('.toggle_btn');
+    const toggleBtnIcon = document.querySelector('.toggle_btn i');
+    const dropDownMenu = document.querySelector('.dropdown_menu');
+
+    toggleBtn?.addEventListener('click', () => {
+      dropDownMenu?.classList.toggle('open');
+
+      const isOpen = dropDownMenu?.classList.contains('open');
+
+      if (toggleBtnIcon) {
+        if (isOpen) {
+          toggleBtnIcon.classList.remove('bx-menu');
+          toggleBtnIcon.classList.add('bx-x');
+        } else {
+          toggleBtnIcon.classList.remove('bx-x');
+          toggleBtnIcon.classList.add('bx-menu');
+        }
+      }
+    });
   }
 
 }
