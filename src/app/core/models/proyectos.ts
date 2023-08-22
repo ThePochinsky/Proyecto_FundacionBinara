f034e21bbadc7ae415301b7f33c7a469c9bf5169
Christian1910n <christian.garaicoa.est@tecazuay.edu.ec>
2023-08-22 15:37:44 -0500

    Get Proyectos Activos

--- src/app/core/models/proyectos.ts
+++ src/app/core/models/proyectos.ts
@@ -1,10 +1,12 @@
+import { Mapas } from "./mapas";
+
 export class Proyectos {
 
     id?: number;
     titulo?: string;
     objetivoPrincipal?: string;
     objetivosSecundarios?: string;
-    mapas?: any;
+    mapas?: Mapas[];
     parrafoUno?: string;
     parrafoDos?: string;
     parrafoTres?: string;
